91cf754be9b833622bde877084c0a2f442c74f40
mikepaxton <mike.paxton@me.com>
2023-08-01 15:51:05 -0700

    Modified code to allow while loop to continue running while linear actuator is running. The new code uses a variable (actuator_running) to check the status of the actuator.  If an event triggers the actuator to run, an actuator_stop_time is calculated based on current time.(monotonic) plus duration(actuator_run_time). The while loop then checks the current time.monotonic() of each consecutive iteration of the loop and if greater than actuator_stop_time executes actuator_stop(). This allows the while loop to continue without pause. Implemented the e-stop (emergency stop) button in case a chicken tries to dart through while door is closing.

--- main.py
+++ main.py
@@ -18,7 +18,13 @@ A relay has been added along with a constant on switch which turns on a light in
 NOTE:  The relay I'm using is triggered in a High state.
 
 08/01/23 - Mike Paxton
-Modified code to not tie up the while loop with a time.sleep() duration for both opening and closing the coop door.
+Modified code to not tie up the while loop with the (actuator_run_time) which waited for actuator to open or close
+before continuing to execute additional code. This prevented any button clicks or other code from executing until the
+actuator_run_time expired.
+The new code uses a variable (actuator_running) to check the status of the actuator.  If an event triggers the actuator
+to run, an actuator_stop_time is calculated based on current time.(monotonic) plus duration(actuator_run_time).
+The while loop then checks the current time.monotonic() of each consecutive iteration of the loop and if greater than
+actuator_stop_time executes actuator_stop(). This allows the while loop to continue without pause.
 Implemented the e-stop (emergency stop) button in case a chicken tries to dart through while door is closing.
 """
 
@@ -77,31 +83,38 @@ day_threshold = 20000
 
 #  Open door routine
 def actuator_open(duration):
-    start_time = time.monotonic()
-    while (time.monotonic() - start_time) < duration:
-        in1.value = True
-        in2.value = False
+    global actuator_running, actuator_stop_time
+    actuator_stop_time = time.monotonic() + duration
+    actuator_running = True
+    in1.value = True
+    in2.value = False
 
 
 # Close door routine
 def actuator_close(duration):
-    start_time = time.monotonic()
-    while (time.monotonic() - start_time) < duration:
-        in1.value = False
-        in2.value = True
+    global actuator_running, actuator_stop_time
+    actuator_stop_time = time.monotonic() + duration
+    actuator_running = True
+    in1.value = False
+    in2.value = True
 
 
 # Stop the door.
 def actuator_stop():
+    global actuator_running
+    actuator_running = False
     in1.value = False
     in2.value = False
 
 
-# On boot open door and set its status to True.
+# On boot open door
 actuator_open(actuator_run_time)
+# We've opened door, set door_open to True for photocell value checking portion of code
 door_open = True
 
+
 while True:
+    # Beginning of open, close and e-stop button code section
     # Check if the "Open" button was pressed
     if not button_open.value:  # Button is active LOW due to pull-up resistor
         print("Opening")
@@ -116,20 +129,26 @@ while True:
     if not button_eStop.value:  # Button is active LOW due to pull-up resistor
         print("E Stop")
         actuator_stop()
+    # End of open, close and e-stop code section
 
-    # Check if the actuator is running and if it's time to stop
-    if actuator_running and time.monotonic() >= actuator_run_time:
-        actuator_running = False
+    # Beginning of actuator running check section of code
+    # If the actuator is running and current time is greater to the calculated stop time then execute code
+    if actuator_running and time.monotonic() > actuator_stop_time:
         print("actuator Stopped")
         actuator_stop()
+        actuator_running = False
+    # End of actuator running check code section
 
+    # Beginning of coop light relay code section
     if not button_light.value:  # Button is active LOW due to pull-up resistor
         light_relay.value = True
         print("Light On")
     else:
         light_relay.value = False
         print("Light Off")
+    # End of coop light relay code section
 
+    # Beginning of Manual override and photocell value checking code section.
     if not button_manualOverride.value:  # Button is active LOW due to pull-up resistor
         led_manualOverride.value = True  # In override mode so turn LED on.
         print("In Manual Override")
@@ -139,14 +158,16 @@ while True:
         led_manualOverride.value = False  # Turn off manual override LED
         if door_open:  # Check door status
             if photocell.value < evening_threshold:  # Door is open, check photocell to see if dark enough to close door
-                actuator_close(actuator_run_time)  # Photocell value is bellow threshold, close door.
                 door_open = False  # Set door status to closed
+                actuator_close(actuator_run_time)  # Photocell value is bellow threshold, close door.
 
         # if we find the photocell has light we open the door.
         if not door_open:
             if photocell.value > day_threshold:  # Door is closed, check photocell to see if light enough to open door
+                door_open = True  # Set door status to open.
                 actuator_open(actuator_run_time)  # Photocell value is above threshold, open door.
-                door_open = True  # Set door status to open
+
+    # End of manual override and photocell value checking code section
 
     time.sleep(0.1)  # Delay for debounce and smoother button handling
 
